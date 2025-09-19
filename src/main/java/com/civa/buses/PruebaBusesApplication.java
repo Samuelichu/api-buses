0900e721ff8daed3d498353dba7c81abb42bbfde
Samuelichu <chavezzsam@gmail.com>
2025-09-19 18:38:05 -0500

    Initial commit

---
+++ src/main/java/com/civa/buses/PruebaBusesApplication.java
@@ -0,0 +1,13 @@
+package com.civa.buses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBusesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBusesApplication.class, args);
+	}
+
+}
